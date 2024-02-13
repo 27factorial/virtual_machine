ceadbb17f32bc21bc8504d0f6e49c56d38f1a1c4
Factorial <27factorial@gmail.com>
2024-02-12 18:23:23 -0800

    update

--- src/object/std_impls.rs
+++ src/object/std_impls.rs
@@ -1,21 +1,23 @@
-use super::VmObject;
+use crate::value::Value;
+
+use super::{VmObject, VmType};
 
 impl VmObject for String {
-    fn register_type(program: &mut crate::program::Program) -> Option<&super::VmType>
+    fn register_type(program: &mut crate::program::Program) -> Option<&VmType>
     where
         Self: Sized {
         todo!()
     }
 
-    fn field(&self, name: &str) -> Option<&crate::value::Value> {
+    fn field(&self, name: &str) -> Option<&Value> {
         todo!()
     }
 
-    fn field_mut(&mut self, name: &str) -> Option<&mut crate::value::Value> {
+    fn field_mut(&mut self, name: &str) -> Option<&mut Value> {
         todo!()
     }
 
-    fn fields(&self) -> &[crate::value::Value] {
+    fn fields(&self) -> &[Value] {
         todo!()
     }
 }
